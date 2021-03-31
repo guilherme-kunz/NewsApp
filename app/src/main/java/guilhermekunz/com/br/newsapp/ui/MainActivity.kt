3311dd7f76f59ecb97855ae36437aa73599383d3
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2021-03-31 13:18:31 -0300

    Implementing Camera and Gallery Access

--- app/src/main/java/guilhermekunz/com/br/newsapp/ui/MainActivity.kt
+++ app/src/main/java/guilhermekunz/com/br/newsapp/ui/MainActivity.kt
@@ -1,19 +1,34 @@
 package guilhermekunz.com.br.newsapp.ui
 
+import android.app.Activity
+import android.content.Intent
+import android.icu.text.SimpleDateFormat
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import de.hdodenhof.circleimageview.CircleImageView
 import guilhermekunz.com.br.newsapp.R
 import guilhermekunz.com.br.newsapp.database.ArticleDatabase
 import guilhermekunz.com.br.newsapp.repository.NewsRepository
 import guilhermekunz.com.br.newsapp.ui.viewmodel.NewsViewModel
 import guilhermekunz.com.br.newsapp.ui.viewmodel.NewsViewModelProviderFactory
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_header.*
+import java.io.File
+import java.io.IOException
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,9 +36,15 @@ class MainActivity : AppCompatActivity() {
     lateinit var viewModel: NewsViewModel
     lateinit var toggle: ActionBarDrawerToggle
 
+    var currentPath: String? = null
+    val TAKE_PICTURE = 1
+    val SELECT_PICTURE = 2
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         //Instancia o Repositorio
         val newsRepository = NewsRepository(ArticleDatabase(this))
@@ -43,10 +64,8 @@ class MainActivity : AppCompatActivity() {
 
         navView.setNavigationItemSelectedListener {
             when(it.itemId) {
-                R.id.Item1 -> Toast.makeText(applicationContext,
-                        "Clicked Item 1", Toast.LENGTH_SHORT).show()
-                R.id.Item2 -> Toast.makeText(applicationContext,
-                        "Clicked Item 2", Toast.LENGTH_SHORT).show()
+                R.id.Item1 -> dispatchCameraIntent()
+                R.id.Item2 -> dispatchGalleryIntent()
                 R.id.Item3 -> Toast.makeText(applicationContext,
                         "Clicked Item 3", Toast.LENGTH_SHORT).show()
             }
@@ -54,10 +73,90 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+//    private fun selectImage() {
+//        val items = arrayOf<CharSequence>("Take Photo", "Choose from Gallery", "Cancel")
+//        val builder = AlertDialog.Builder(this@MainActivity)
+//        builder.setItems(items) { dialog, item ->
+//            when {
+//                items[item] == "Take Photo" -> {
+//                    requestStoragePermission(true)
+//                }
+//                items[item] == "Choose from Library" -> {
+//                    requestStoragePermission(false)
+//                }
+//                items[item] == "Cancel" -> {
+//                    dialog.dismiss()
+//                }
+//            }
+//        }
+//        builder.show()
+//    }
+
+    // recebe a foto
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TAKE_PICTURE && resultCode == Activity.RESULT_OK) {
+            try {
+                val file = File(currentPath)
+                val uri = Uri.fromFile(file)
+                profile_image.setImageURI(uri)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        if (requestCode == SELECT_PICTURE && resultCode == Activity.RESULT_OK) {
+            try {
+                val uri = data!!.data
+                profile_image.setImageURI(uri)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    //abre a galeria
+    private fun dispatchGalleryIntent(){
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select image"), SELECT_PICTURE)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)) {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    //abre a camera
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun dispatchCameraIntent() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            var photoFile: File? = null
+            try {
+                photoFile = createImage()
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+            if (photoFile != null) {
+                var photoUri = FileProvider.getUriForFile(this,
+                        "guilhermekunz.com.br.newsapp.fileprovider", photoFile)
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                startActivityForResult(intent, TAKE_PICTURE)
+            }
+        }
+    }
+
+    //cria a imagem
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun createImage(): File{
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageName = "JPEG_"+timeStamp+"_"
+        var storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        var image = File.createTempFile(imageName, ".jpg", storageDir)
+        currentPath = image.absolutePath
+        return image
     }
 }
